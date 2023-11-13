233293d83a041ade79d908df085c3c92f91e1277
CaramonH <Caramonh@yahoo.com>
2023-11-13 14:54:43 -0600

    Task 0

--- Javascript_advanced/0-welcome.js
+++ Javascript_advanced/0-welcome.js
@@ -1,6 +1,6 @@
 function welcome(firstName, lastName)
 {
-    let fullName =firstName + " " + lastName;
+    let fullName = firstName + " " + lastName;
 
     function displayFullName() {
         alert("Welcome " + fullName + "!");
