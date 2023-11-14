093efa161f3561d33cd1d62cd1d446e3af4199e7
CaramonH <Caramonh@yahoo.com>
2023-11-14 14:31:39 -0600

    Oops forgot to push

--- Javascript_advanced/6-hogwarts.js
+++ Javascript_advanced/6-hogwarts.js
@@ -21,3 +21,18 @@ function studentHogwarts() {
     }
   };
 }
+
+const harry = new studentHogwarts();
+harry.setName('Harry');
+for (let i = 0; i < 4; i++) {
+  harry.rewardStudent();
+}
+console.log(harry.getScore());
+
+const draco = new studentHogwarts();
+draco.setName('Draco');
+draco.rewardStudent();
+for (let i = 0; i < 3; i++) {
+  draco.penalizeStudent();
+}
+console.log(draco.getScore());
---
+++ Javascript_advanced/8-paymetns.js
@@ -0,0 +1,19 @@
+function processPayment(amount) {
+  console.log("Collecting payment of " + amount);
+}
+
+function processOrder(orderID, amount) {
+  console.log(orderID + ' is being processed');
+  processPayment(amount);
+  console.log(orderID + ' has been fully processed');
+}
+
+function main() {
+  console.log("Processing orders");
+  processOrder(12321, 10.99);
+  processOrder(12322, 12.99);
+  processOrder(12323, 15.00);
+  console.log("All the orders have been processed");
+}
+
+main();
