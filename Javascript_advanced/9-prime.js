e07153f31ef57767d29e7091d66491867c385942
CaramonH <Caramonh@yahoo.com>
2023-11-14 14:46:22 -0600

    8 & 9

---
+++ Javascript_advanced/9-prime.js
@@ -0,0 +1,29 @@
+function countPrimeNumbers() {
+  let count = 0;
+
+  for (let i = 2; i <= 100; i++) {
+    if (isPrime(i)) {
+      count++;
+    }
+  }
+  return count;
+};
+
+const isPrime = (num) => {
+  for (let i = 2; i< num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
+};
+
+const time1 = performance.now();
+countPrimeNumbers();
+const time2 = performance.now();
+
+console.log(
+  `Execution time of printing countPrimeNumbers was ${
+    time2 - time1
+  } milliseconds.`
+);
