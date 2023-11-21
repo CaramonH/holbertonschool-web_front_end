16a4b63944fd3d8abd7aa2a571603b9c6f07c48e
CaramonH <Caramonh@yahoo.com>
2023-11-21 16:46:05 -0600

    Task 1

---
+++ Cookies_local_storage/1.js
@@ -0,0 +1,16 @@
+const setCookies = () => {
+  let date = new Date();
+  date.setTime(date.getTime() + 1 * 24 * 60 * 60 * 1000);
+  date = date.toUTCString();
+
+  const firstName = document.getElementById('firstname').value;
+  document.cookie = `firstname=${firstName}; expires=${date}`;
+  const email = document.getElementById('email').value;
+  document.cookie = `email=${email}; expires=${date}`;
+};
+
+const showCookies = () => {
+  const unhide = document.createElement('p');
+  unhide.innerHTML = `<p>Cookies: ${document.cookie}</p>`;
+  document.body.appendChild(unhide);
+};
